b7059bf409ea329aa1c1377d659e52ffaffa1500
DanielG2602 <daniel.comercial0981@.gmail.com>
2024-12-24 00:51:38 -0300

    TwoSum 2

---
+++ problem_list.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l = 0
+        r = len(numbers) - 1
+
+        while l < r:
+            _sum = numbers[l] + numbers[r]
+            if _sum == target:
+                return [l + 1, r + 1]
+            
+            if _sum > target:
+                r -= 1
+            else:
+                l += 1
+        return []
