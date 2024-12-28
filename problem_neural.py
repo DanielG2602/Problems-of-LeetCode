02c155394d96597c98bf161c007935eda2cbf321
DanielG2602 <daniel.comercial0981@.gmail.com>
2024-12-28 17:33:58 -0300

    Rede neural

---
+++ problem_neural.py
@@ -0,0 +1,8 @@
+# para que dê certo a rede neural instale o tensorFlow com o comando abaixo
+# pip install tensorflow keras
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+model = Sequential([Dense(4, input_dim=2, activation='relu'), Dense(1, activation='sigmoid')])
+model.compile(optimizer='adam', loss='binary_crossentropy')
