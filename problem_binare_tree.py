da2ed87e784827b70f6e808c174f634a052c13f5
DanielG2602 <daniel.comercial0981@.gmail.com>
2025-02-19 21:11:29 -0300

    Binare Tree

---
+++ problem_binare_tree.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+class ArvoreNodulo :
+    def __init__(self, val=0, left=None, right=None):
+        self.val
+        self.left
+        self.right
+        
+class Solucao:
+    def inverterArvore(self, root: Optional[ArvoreNodulo]) -> Optional[ArvoreNodulo]:
+        
+        def invercionar(root):
+            if root:
+                root.left, root.right = root.right, root.left
+                invercionar(root.left), invercionar(root.right)
+                
+        invercionar(root)
+        return root
+    
+     def imprimirEmOrdem(self, root: Optional[ArvoreNodulo]):
+        if root:
+            self.imprimirEmOrdem(root.left)
+            print(root.val, end=" ")
+            self.imprimirEmOrdem(root.right)
