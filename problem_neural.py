3eaefb19396e24e68d3dbf4946bdcf1b3ff5a3c3
DanielG2602 <daniel.comercial0981@.gmail.com>
2025-04-20 19:27:18 -0300

    Search-Binary

--- problem_neural.py
+++ problem_neural.py
@@ -1,7 +1,6 @@
 # para que dê certo a rede neural instale o tensorFlow com o comando abaixo
 # pip install tensorflow keras
 
-
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 model = Sequential([Dense(4, input_dim=2, activation='relu'), Dense(1, activation='sigmoid')])
